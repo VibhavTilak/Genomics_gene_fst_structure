85decc5b332585331ae42214e2871434395c48a0
Vibhav-Sanglipincode <vibhavtilak23@gmail.com>
2026-03-30 14:23:23 +0530

    run this on terminal in the directory where pruned.beagle.gz files are saved

---
+++ 11.1_pcacov_generate.sh
@@ -0,0 +1,4 @@
+pcangsd \
+  -b /media/birdlab/bird_hdd_13/Vibhav/tawny/08_structure/all_pops.pruned.beagle.gz \
+  -o /media/birdlab/bird_hdd_13/Vibhav/tawny/08_structure/all_pops_pcangsd \
+  -t 20
